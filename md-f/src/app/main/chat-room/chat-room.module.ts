b6f6700c1d785f6eb8dc641bd2b607ad113f2631
vinaykumar0459 <vinaykumar0459@gmail.com>
2018-04-13 14:54:42 +0530

    navicons-done

--- md-f/src/app/main/chat-room/chat-room.module.ts
+++ md-f/src/app/main/chat-room/chat-room.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { ChatRoomComponent } from './chat-room.component';
 import { ChatRoomRoutingModule } from './chat-room.routing';
 import { CommonModule } from '@angular/common';
+import { FriendsListComponent } from './friends-list/friends-list.component';
+import { ChatBoxComponent } from './chat-box/chat-box.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
     imports:[
         ChatRoomRoutingModule,
-        CommonModule
+        CommonModule,
+        FlexLayoutModule
     ],
     declarations: [ 
-        ChatRoomComponent,
+        ChatRoomComponent, FriendsListComponent, ChatBoxComponent,
     ],
     providers: [
         
--- md-f/src/app/main/main.module.ts
+++ md-f/src/app/main/main.module.ts
@@ -3,11 +3,13 @@ import { MainComponent } from './main.component';
 import { MainRoutingModule } from './main.routing';
 import { CommonModule } from '@angular/common';
 import { NavIconsComponent } from './nav-icons/nav-icons.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
     imports:[
         MainRoutingModule,
-        CommonModule
+        CommonModule,
+        FlexLayoutModule
     ],
     declarations: [ 
         MainComponent, NavIconsComponent
